2b8a0bb38aab0efecab8c87324758cb1185a6bc7
Thai Nguyen <thai2862005@gmail.com>
2025-11-22 21:59:55 +0700

    Simplify Prisma client import to use standard @prisma/client export

--- src/config/client.ts
+++ src/config/client.ts
@@ -1,15 +1,7 @@
 import 'dotenv/config'
-import * as PrismaPkg from '@prisma/client'
+import { PrismaClient } from '@prisma/client'
 import { PrismaPg } from '@prisma/adapter-pg'
 import { Pool } from 'pg'
-
-// Support different Prisma client module shapes by resolving PrismaClient at runtime
-const _PrismaPkgAny = PrismaPkg as any
-const PrismaClient = _PrismaPkgAny.PrismaClient || _PrismaPkgAny.default?.PrismaClient
-
-if (!PrismaClient) {
-  throw new Error('PrismaClient not found in @prisma/client package')
-}
 
 const connectionString = process.env.DATABASE_URL || ''
 
